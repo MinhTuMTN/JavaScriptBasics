9a84794660920e1cd79fea751e68d100df773b30
Minh Tu Nguyen <88047847+MinhTuMTN@users.noreply.github.com>
2023-04-07 01:51:16 +0700

    Thêm ví dụ ++ --

--- 14-27. Toán tử/main.js
+++ 14-27. Toán tử/main.js
@@ -1,4 +1,4 @@
-// Toán tử số học
+//// Toán tử số học
 
 var a = 5;
 var b = 6;
@@ -22,4 +22,32 @@ var c = 8;
 console.log(c++);
 console.log(c);
 
-// Toán tử gán
+//// Toán tử gán
+
+//// Khác biệt giữa ++a và a++
+console.log("--- Hậu tố ---");
+
+var number1 = 1;
+
+console.log(number1++); // 1
+console.log(number1); // 2
+
+console.log(number1++); // 2
+console.log(number1); // 3
+
+console.log("--- Tiền tố ---");
+
+var number2 = 1;
+
+console.log(++number2); // 2
+console.log(number2); // 2
+
+console.log(++number2); // 3
+console.log(number2); // 3
+
+// Toán tử -- sẽ tương tự
+// Một số ví dụ khác
+
+var a = 3;
+var b = ++a * 2 + a++;
+// b = 4*2 + 4 = 12
